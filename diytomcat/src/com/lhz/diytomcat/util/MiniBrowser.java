4cd4b8f5dcc908e6cae8d4e55ea159b9db1165a2
lhz <2190732276@qq.com>
2020-05-27 19:31:29 +0800

    模拟浏览器客户端02

--- diytomcat/src/com/lhz/diytomcat/util/MiniBrowser.java
+++ diytomcat/src/com/lhz/diytomcat/util/MiniBrowser.java
@@ -1,10 +1,17 @@
 package com.lhz.diytomcat.util;
 
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author: lhz
@@ -27,7 +34,59 @@ public class MiniBrowser {
         return null;
     }
 
-    public static String getHttpBytes(String url, boolean gzip) {
+    public static String getContentString(String url) {
+
+    }
+
+    public static String getContentString(String url, boolean gzip) {
+        byte[] result = getContentBytes(url, gzip);
+        if (null == result) {
+            return null;
+        }
+        try {
+            return new String(result,"utf-8").trim();
+        } catch (UnsupportedEncodingException e) {
+            return null;
+        }
+    }
+
+    /**
+     * 获取二进制的相应体
+     * @param url
+     * @param gzip
+     * @return
+     */
+    public static byte[] getContentBytes(String url, boolean gzip) {
+        byte[] response = getHttpBytes(url,gzip);
+        byte[] doubleReturn = "\r\n\r\n".getBytes();
+
+        int pos = -1;
+        for (int i = 0; i < response.length-doubleReturn.length; i++) {
+            byte[] temp = Arrays.copyOfRange(response, i, i + doubleReturn.length);
+
+            if(Arrays.equals(temp, doubleReturn)) {
+                pos = i;
+                break;
+            }
+        }
+        if (-1 == pos) {
+            return null;
+        }
+
+        pos += doubleReturn.length;
+
+        byte[] result = Arrays.copyOfRange(response, pos, response.length);
+        return result;
+    }
+
+
+    /**
+     * 返回二进制的 http 响应头
+     * @param url
+     * @param gzip
+     * @return
+     */
+    public static byte[] getHttpBytes(String url, boolean gzip) {
         byte[] result = null;
         try {
             URL u = new URL(url);
@@ -38,13 +97,58 @@ public class MiniBrowser {
             }
             InetSocketAddress inetSocketAddress = new InetSocketAddress(u.getHost(),port);
             client.connect(inetSocketAddress,1000);
+            //封装http请求头
             Map<String,String> requestHeaders = new HashMap<>();
             requestHeaders.put("Host", u.getHost()+":"+port);
             requestHeaders.put("Accept", "text/html");
             requestHeaders.put("Connection", "close");
             requestHeaders.put("User-Agent", "lhz mini brower / java1.8");
+
+            if (gzip) {
+                requestHeaders.put("Accept-Encoding", "gzip");
+            }
+            String path = u.getPath();
+            if (path.length() == 0) {
+                path = "/";
+            }
+            //请求头
+            String fristLine = "GET " + path + "HTTP/1.1\r\n";
+
+            StringBuffer httpRequestString = new StringBuffer();
+            httpRequestString.append(fristLine);
+            Set<String> headers = requestHeaders.keySet();
+            for (String header : headers) {
+                String headerLine = header + ": " + requestHeaders.get(header) + "\r\n";
+                httpRequestString.append(headerLine);
+            }
+            //封装好了 写出
+            PrintWriter writer = new PrintWriter(client.getOutputStream());
+            writer.println(httpRequestString);
+            InputStream is = client.getInputStream();
+            int buffer_size = 1024;
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            byte[] buffer = new byte[buffer_size];
+            while (true) {
+                int length = is.read(buffer);
+                if (-1 == length) {
+                    break;
+                }
+            }
+            int len = 0;
+            while ((len=is.read(buffer)) != -1) {
+                baos.write(buffer,0,len);
+                if (len != buffer_size) {
+                    break;
+                }
+            }
+            result = baos.toByteArray();
+            client.close();
+
         } catch (Exception e) {
             e.printStackTrace();
+            result = e.toString().getBytes(StandardCharsets.UTF_8);
         }
+        return result;
     }
 }
